dc2ea49886d703a29d5c27069b23e646b6da8c8a
Imran-Ifty <63757018+Imran-Ifty@users.noreply.github.com>
2024-02-08 19:52:28 +0600

    asd

---
+++ README.md
@@ -0,0 +1,21 @@
+<h1 align="center">Hi 👋, I'm Imran Ifty</h1>
+<h3 align="center">A passionate Game Developer From Bangladesh</h3>
+
+<p align="left"> <a href="https://github.com/ryo-ma/github-profile-trophy"><img src="https://github-profile-trophy.vercel.app/?username=imran-ifty" alt="imran-ifty" /></a> </p>
+
+- 🔭 I’m currently working on **TraPing: A PingPong and Pacman Game Combo**
+
+- 🌱 I’m currently learning **.net ,c++ , flutter**
+
+- 💬 Ask me about **Java , C++ , Python**
+
+<h3 align="left">Connect with me:</h3>
+<p align="left">
+</p>
+
+<h3 align="left">Languages and Tools:</h3>
+<p align="left"> <a href="https://developer.android.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/android/android-original-wordmark.svg" alt="android" width="40" height="40"/> </a> <a href="https://www.w3schools.com/cpp/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" alt="cplusplus" width="40" height="40"/> </a> <a href="https://flutter.dev" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/flutterio/flutterio-icon.svg" alt="flutter" width="40" height="40"/> </a> <a href="https://www.java.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" alt="java" width="40" height="40"/> </a> <a href="https://www.linux.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg" alt="linux" width="40" height="40"/> </a> <a href="https://www.mysql.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="40" height="40"/> </a> <a href="https://nodejs.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40"/> </a> <a href="https://opencv.org/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/opencv/opencv-icon.svg" alt="opencv" width="40" height="40"/> </a> <a href="https://www.php.net" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg" alt="php" width="40" height="40"/> </a> <a href="https://www.python.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a> <a href="https://unity.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/unity3d/unity3d-icon.svg" alt="unity" width="40" height="40"/> </a> </p>
+
+<p><img align="left" src="https://github-readme-stats.vercel.app/api/top-langs?username=imran-ifty&show_icons=true&locale=en&layout=compact" alt="imran-ifty" /></p>
+
+<p>&nbsp;<img align="center" src="https://github-readme-stats.vercel.app/api?username=imran-ifty&show_icons=true&locale=en" alt="imran-ifty" /></p>
---
+++ scripts/script.js
@@ -0,0 +1,159 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const inputField = document.querySelector('.Prompt__input');
+    const terminalText = document.querySelector('.Terminal__text');
+    let commandCount = 0; // Variable to track the command count
+  
+    inputField.addEventListener('keydown', function (e) {
+      if (e.key === 'Enter') {
+        const command = inputField.value.trim();
+        inputField.value = ''; // Clear input field after entering command
+  
+        if (command === 'clear') {
+          clearTerminal();
+        } else {
+          displayCommandResult(command);
+        }
+      }
+    });
+  
+    function displayCommandResult(command) {
+      // Increment the command count
+      commandCount++;
+      // Create a new command prompt and display the entered command
+      let newCommandPrompt = '';
+      switch (command) {
+        case "ls":
+          newCommandPrompt += `<div class = "prompt"><span 
+                    class="GreenText">Imran@Ifty:~$ 
+                    </span> ${command}</div>`;
+          terminalText.innerHTML += newCommandPrompt;
+  
+          // Add clickable letters under the terminal text
+          terminalText.innerHTML += `<div><span class="ClickableLetter">Projects</span> 
+                  <span         
+                  class="ClickableLetter">Skills</span> <span class="ClickableLetter">Github</span> 
+                  <span 
+                  class="ClickableLetter">About</span></div><br>`;
+          break;
+  
+        case "pwd":
+          newCommandPrompt += `<div class = "prompt"><span 
+                    class="GreenText">Imran@Ifty:~$ 
+                    </span> ${command}</div>`;
+          terminalText.innerHTML += newCommandPrompt;
+  
+          terminalText.innerHTML += `<div>home/Imran-Ifty</div><br>`;
+          break;
+  
+        case "whoami":
+          newCommandPrompt += `<div class = "prompt"><span 
+                    class="GreenText">Imran@Ifty:~$ 
+                    </span> ${command}</div>`;
+          terminalText.innerHTML += newCommandPrompt;
+  
+          terminalText.innerHTML += `<div>Imran-Ifty</div><br>`;
+          break;
+        case "cat":
+          newCommandPrompt += `<div class = "prompt"><span 
+                    class="GreenText">Imran@Ifty:~$ 
+                    </span> ${command}</div>`;
+          terminalText.innerHTML += newCommandPrompt;
+  
+          terminalText.innerHTML += `<div><span 
+                  class="ClickableLetter">Meeeeeeeeeeeeeaaaaaaaaaaaaaaaaooooooowwwwwwww</span>
+                  </div><br>`;
+          break;
+  
+        case "cd Projects":
+        case "cd Skills":
+        case "cd Github":
+        case "cd About":
+          newCommandPrompt += `<div class = "prompt"><span 
+                    class="GreenText">Imran@Ifty:~$ 
+                    </span> ${command}</div>`;
+          terminalText.innerHTML += newCommandPrompt;        
+          const directoryName = command.split("cd ")[1];
+          if(directoryName == "Projects"){
+            terminalText.innerHTML += 
+            `<div> <span>
+                <a href="https://github.com/Imran-Ifty/CSEKU_SDP_2023_TraPing_-_A_PingPong_and_Pacman_game_Combo" class="ClickableLetter" target="_blank">
+                    Traping- A pacman Game
+                </a>
+                </span><br>
+                
+                <span>
+                <a href="https://github.com/Imran-Ifty/wsl-distro-manager" class="ClickableLetter" target="_blank">
+                    WSL Distro Manager
+                </a>
+                </span><br>
+                
+                <span>
+                <a href="https://github.com/Imran-Ifty/Project-Generator" class="ClickableLetter" target="_blank">
+                    PHP-Project Generator
+                </a>
+                </span>     
+
+            </div><br>`;
+          }
+          if(directoryName == "About" || directoryName == "Skills")
+          {
+
+             terminalText.innerHTML +=`<div> <span>
+            <a href="https://github.com/Imran-Ifty/Imran-Ifty/blob/master/README.md" class="ClickableLetter" target="_blank">Readme</a>
+
+            </span><br>
+         </div><br>`;
+        
+          }
+
+          if(directoryName == "About" || directoryName == "Skills")
+          {
+
+             terminalText.innerHTML +=`<div> <span>
+            <a href="
+            https://github.com/Imran-Ifty" class="ClickableLetter" target="_blank">GitHub</a>
+
+            </span><br>
+         </div><br>`;
+        
+          }
+          break;
+        
+        case "":
+          newCommandPrompt += `<div class = "prompt"><span 
+          class="GreenText">Imran@Ifty:~$ 
+          </span> ${command}</div>`;
+          terminalText.innerHTML += newCommandPrompt;
+          terminalText.innerHTML += 
+          `<div><span class="ClickableLetter">
+            Help: command -> cd, ls, pwd, cat
+          </span></div><br>`;
+      }
+  
+      // Scroll to the bottom of the terminal
+      terminalText.scrollTop = terminalText.scrollHeight;
+    }
+  
+    function clearTerminal() {
+      // Clear the terminal
+      terminalText.innerHTML = '';
+      commandCount = 0; // Reset the command count
+    }
+  });
+  
+  function toggleTerminal() {
+    var terminal = document.getElementById("terminal");
+    var terminalApp = document.getElementById("terminalApp");
+  
+    if (terminal.style.display === "none") {
+      terminal.style.display = "block";
+      terminalApp.style.display = "none";
+    } else {
+      terminal.style.display = "none";
+      terminalApp.style.display = "block";
+    }
+  }
+  
+
+
+  
