51811db05cd8c2a12efb45182e7d22d7a6a3a648
Imran-Ifty <63757018+Imran-Ifty@users.noreply.github.com>
2026-06-19 18:22:33 +0600

    asdasd

---
+++ service-worker.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = 'attendance-system-v3';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Return cached version or fetch from network
+                return response || fetch(event.request).then(response => {
+                    // Cache new requests for offline use
+                    if (response.status === 200) {
+                        const responseClone = response.clone();
+                        caches.open(CACHE_NAME).then(cache => {
+                            cache.put(event.request, responseClone);
+                        });
+                    }
+                    return response;
+                });
+            })
+            .catch(() => {
+                // Return offline page if available
+                return caches.match('./index.html');
+            })
+    );
+});
+
+// Update cache
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
